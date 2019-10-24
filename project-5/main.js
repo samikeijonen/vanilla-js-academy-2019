170c4961ebb2d71982f401f97cd6f212d191c608
Sami Keijonen <sami.keijonen@foxnet.fi>
2019-10-24 21:59:13 +0300

    Add word count.

---
+++ project-5/main.js
@@ -0,0 +1,43 @@
+(function() {
+	var text = document.querySelector( '.js-text' );
+
+	// Bail if we don't have textarea field.
+	if ( ! text ) {
+		return;
+	}
+
+	var textCount = document.querySelector( '.js-char-count' );
+	var wordCount = document.querySelector( '.js-word-count' );
+
+	/**
+	 * Calculate textarea lenght and output using textContent.
+	 */
+	var handleInput = function () {
+		// Update char count.
+		var charCount = text.value.length
+		textCount.textContent = charCount;
+
+		// Get words.
+		var words = text.value.split( ' ' );
+
+		// Remove empty arrays, meaning only spaces.
+		var wordsCleaned = words.filter( function ( word ) {
+			return word.length > 0;
+		} );
+
+		// Get count of words.
+		var wordsCount = wordsCleaned.length;
+		wordCount.textContent = wordsCount;
+
+		// Speak the dynamic change.
+		speak( 'You\'ve written ' + wordsCount + ' words and ' + charCount +  ' characters.' );
+	}
+
+	text.addEventListener( 'input', handleInput, false );
+
+	// Create div for speak.
+	addSpeakContainer();
+
+	// Update fields on page load.
+	handleInput();
+})();
