6f918570b09f49249ff00fe42b5065e526e8f4ca
Sami Keijonen <sami.keijonen@foxnet.fi>
2019-11-03 23:45:43 +0200

    Cleaner loops.

--- project-9/main.js
+++ project-9/main.js
@@ -13,17 +13,8 @@
 
 	var topCategories = `${ baseUrl }categories?taxonomy=category&per_page=4&orderby=count&order=desc`;
 
-	// Array for storing top category names retrieved from response.
-	var nameArray = [];
-
-	// Array for storing top category IDs retrieved from response.
-	var idArray = [];
-
-	// Array for storing top category count retrieved from response.
-	var countArray = [];
-
-	// Object for all above info.
-	var allInfo = {};
+	// Array for all info.
+	var allInfo = [];
 
 	/**
 	 * Render article markup.
@@ -31,15 +22,25 @@
 	 * @param {object} data Data to handle.
 	 */
 	var renderArticles = function ( data ) {
+		// Loop all info so that we can get all under same umbrella.
+		var k = 0;
+		allInfo.forEach( function ( field ) {
+			// Add as articles object.
+			field.articles = data[k];
+			k++;
+		} );
+
 		console.log( data, allInfo );
 
 		// Loop articles (nested array).
-		var k = 0;
-		app.innerHTML = data.map( function ( articles ) {
-			k++;
-			return `<h2>${ sanitizeHTML( allInfo.name[k - 1] ) } (${ sanitizeHTML( allInfo.count[k - 1] ) } total)</h2><ul>` + articles.map( function ( article ) {
-				return `<li><a href="${ sanitizeHTML( article.link ) }">${ sanitizeHTML( article.title.rendered ) }</a></li>`;
-			} ).join( '' ) + '</ul>';
+		app.innerHTML = allInfo.map( function ( info ) {
+			return `
+				<h2><a href="${ sanitizeHTML( info.link ) }">${ sanitizeHTML( info.name ) }</a> (${ sanitizeHTML( info.count ) } total)</h2>
+				<ul>` +
+					info.articles.map( function ( article ) {
+						return `<li><a href="${ article.link }">${ article.title.rendered }</a></li>`;
+					} ).join( '' ) +
+				`</ul>`;
 		} ).join( '' );
 	};
 
@@ -59,36 +60,20 @@
 		} ).then( function ( data ) {
 			// This is the JSON from our response.
 			console.log( data );
-
-			if ( data.length > 0 ) {
-				// Push names.
-				data.map( item => nameArray.push( item.name ) );
-
-				// Push IDs.
-				data.map( item => idArray.push( item.id ) );
-
-				// Push counts.
-				data.map( item => countArray.push( item.count ) );
-			}
-			console.log( nameArray, idArray, countArray );
-
-			// Return object of all info.
-			allInfo = {
-				name: nameArray,
-				id: idArray,
-				count: countArray,
-			}
-			console.log( allInfo );
-			return allInfo;
+			allInfo = data;
+			return data;
 		} ).then( function ( info ) {
-			console.log( info, info.id );
+			console.log( info );
 			// Return an promise which will return "JSON response" array for all IDs.
 			// Promise.all means "Wait for these things", not "Do these things".
-			return Promise.all( info.id.map( id => {
+			var all = Promise.all( info.map( item => {
 				// Fetch articles based on category ID, return JSON response.
-				return fetch( `${ baseUrl }posts?per_page=4&categories=${ id }` ).then( res => res.json() );
+				return fetch( `${ baseUrl }posts?per_page=4&categories=${ item.id }` ).then( res => res.json() );
 			} ) );
+
+			return all;
 		 } ).then( function ( res ) {
+			 console.log( res );
 			// Render articles.
 			renderArticles( res );
 			return res;
